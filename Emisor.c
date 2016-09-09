861ebeef2fd315b23872d7c8acecef87b3adac8e
Esdras Altamirano <altamiranoesdras@gmail.com>
2016-09-09 09:03:36 -0600

    Prueba inicial

--- Emisor.c
+++ Emisor.c
@@ -1,32 +1,26 @@
+#define __LINUX_COM__
 #include "com/serial.h"
 
- HANDLE fd;  //manejador del puerto
- DCB OldConf; //Estructura de configuracion del puerto
- 
- //******************************************* procedimiento abrir_puerto_serie ****************************************
 
-void abrir_puerto_serie(){
-	fd=Open_Port("/dev/ttyS0");         // Abre el puerto serie        
-    	OldConf=Get_Configure_Port(fd);     // guardo la configuracion del puerto 
-    	Configure_Port(fd,B115200,"8N1");   // Configuro el puerto serie 
-}
+int main(int argc, char const *argv[]){
 
+	HANDLE fd;
+    DCB OldConf;
+    char cad[16]="Enviando Texto";
+    int n;
 
-//******************************************* procedimiento cerrar_puerto_serie ****************************************
-
-void cerrar_puerto_serie(){
-    	Set_Configure_Port(fd,OldConf);     // Restituyo la antigua configuracion del puerto 
-    	Close_Port(fd); 		    // Cierro el puerto serie     
-}
-
-main()
-{
-	abrir_puerto_serie();	
-	while(hay_algo_que_leer_del_archivo){
-		char paquete[1024];
-		paquete=leer_1024_bytes_del_archivo();
-		n=Write_Port(fd,paquete,1024);	 // Escribo en el puerto serie
+	if (argc<2){
+		printf("Faltan parametros de conexión\n");
+		return 0;
 	}
-	cerrar_puerto_serie();
+
+	printf("Abriendo puerto serie...\n");
 	
+	fd=Open_Port((char*) argv[1]);         
+  
+	printf("Configurando puerto serie...\n");
+	Configure_Port(fd,B9600,(char*) "8N1");   
+    
+	/* code */
+	return 0;
 }
