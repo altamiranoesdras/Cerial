ea70cc12ca16ec2bec8611657e5cc77d3aaac5f3
Esdras Altamirano <altamiranoesdras@gmail.com>
2016-09-09 11:19:35 -0600

    Emisor funcionando mediante argumentos (solo envía texto)

--- Emisor.c
+++ Emisor.c
@@ -1,26 +1,123 @@
 #define __LINUX_COM__
 #include "com/serial.h"
+#include <stdio.h>    // Libreria estanda entrada y salida
+#include <unistd.h>   // para utilizar usleep()
+#include <getopt.h>   // para leer la opciones que se pasan como argumentos
+#include <string.h>   // para manejar cadenas de texto
+#include <stdlib.h>
 
 
-int main(int argc, char const *argv[]){
+void error(char* msg){
+    fprintf(stderr, "%s\n",msg);
+    exit(EXIT_FAILURE);
+}
 
-	HANDLE fd;
-    DCB OldConf;
-    char cad[16]="Enviando Texto";
-    int n;
+void modoUso(void){
+    printf("Modo de uso: ./Emisor -b <bps> -p <puerto> [Opciones]\n"
+    "\n"
+    "Opciones:\n"
+    "  -h, --help                 Imprime modo de uso\n"
+    "  -b, --baud=baudrate        Baudrate (bps) (velocidad) (9600 por defecto)\n"
+    "  -p, --port=serialport      Puerto o adaptador conectado (\"/dev/ttyUSB0\")\n"
+    "  -s, --send=string          Envia una cadena de texto\n"
+    "  -f, --sendFile=ruta            Envia un archivo de texto\n"
+ 
+    "\n"
+    "Nota: El orden es importante. Configure '-b' baudrate antes de arbrir el puerto '-p'. \n"
+    "\n");
+    exit(EXIT_SUCCESS);
+}
 
-	if (argc<2){
+int main(int argc, char *argv[]){
+
+	const int bufmax = 256;//maximo tamaño de las cadenas
+
+	HANDLE fd = -1; //manejador
+    DCB OldConf; //configuracion antigua
+   	
+    char strport[bufmax]="/dev/ttyUSB0";
+    int  velocidad = 9600;  // default
+   	char texto[bufmax]="Hola Mundo";
+   	char rutaArchivo[bufmax]="";
+    
+    
+
+	if (argc==1){
+		modoUso();
 		printf("Faltan parametros de conexión\n");
-		return 0;
 	}
 
-	printf("Abriendo puerto serie...\n");
-	
-	fd=Open_Port((char*) argv[1]);         
-  
-	printf("Configurando puerto serie...\n");
-	Configure_Port(fd,B9600,(char*) "8N1");   
-    
-	/* code */
-	return 0;
+	/* Parseo de opciones */
+    int option_index = 0, opt;
+    static struct option loptions[] = {
+        {"help",       no_argument,       0, 'h'},
+        {"port",       required_argument, 0, 'p'},
+        {"baud",       required_argument, 0, 'b'},
+        {"send",       required_argument, 0, 's'},
+        {"sendFile",   required_argument, 0, 'f'},
+        {NULL,         0,                 0, 0}
+    };
+
+    while(1) {
+
+    	//le los argumetos y/o opciones
+        opt = getopt_long (argc, argv, "hp:b:s:f:",loptions, &option_index);
+
+        //cuando ya no hay mas opciones o argumento que ler
+        if (opt==-1) break;
+
+        switch (opt) {
+	        case '0': break;
+	 
+	        case 'h':
+	            modoUso();
+	        break;
+
+	        case 'b':
+	            velocidad = strtol(optarg,NULL,10);
+	        break;
+
+	        case 'p':
+
+	        	//si el puerto esta abierto
+		        if( fd!=-1 ) {
+	                //Set_Configure_Port(fd,OldConf);//restaura configuracion        
+    				Close_Port(fd);
+	                printf("Cerando puerto... %s\n",strport);
+	            }    
+
+	            strcpy(strport,optarg);
+	            fd=Open_Port(strport); 
+	            //OldConf=Get_Configure_Port(fd); // guardo la configuracion del puerto 
+
+	            //si no se pudo abrir el puerto
+	            if (fd == -1) exit(EXIT_SUCCESS);
+
+	            Configure_Port(fd,velocidad,(char*) "8N1");   
+
+	        break;
+	        
+	        case 's':
+	        	if( fd == -1 ) error((char*) "El puerto serial no esta abierto");
+
+	        	strcpy(texto,optarg);
+	        	printf("Enviando texto: %s ...\n",texto);
+
+	        	Write_Port(fd,texto,bufmax);            
+	            Close_Port(fd);
+	            
+            break;
+
+            case 'f':
+				if( fd == -1 ) error((char*) "El puerto serial no esta abierto");
+
+	        	strcpy(rutaArchivo,optarg);
+	        	printf("Enviando archivo: %s ...\n",rutaArchivo);
+	            
+            break;
+        
+        }
+    }
+
+    exit(EXIT_SUCCESS);
 }
